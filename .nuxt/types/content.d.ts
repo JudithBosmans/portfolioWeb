6ba73d4b869e9db721136bb74d1f807e2180dc0d
Judith Bosmans <judobosmans@gmail.com>
2024-10-11 11:49:42 +0200

    update - 01 init

---
+++ .nuxt/types/content.d.ts
@@ -0,0 +1,4 @@
+declare module '#content/server' {
+  const serverQueryContent: typeof import('/Users/judith.b/Desktop/24-25/LUCA/WEB-DESIGN/portfolioWeb/node_modules/@nuxt/content/dist/runtime/legacy/types').serverQueryContent
+  const parseContent: typeof import('/Users/judith.b/Desktop/24-25/LUCA/WEB-DESIGN/portfolioWeb/node_modules/@nuxt/content/dist/runtime/server').parseContent
+}
