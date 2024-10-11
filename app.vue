446d94932f368d2ac98c47d620e4e521fa918b25
Judith Bosmans <91527023+JudithBosmans@users.noreply.github.com>
2024-10-11 11:43:48 +0200

    Add files via upload

---
+++ app.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="w-full h-full no-print">
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+  </div>
+</template>
+
+<script setup>
+useHead({
+  title: "JB Portfolio",
+  meta: [{ name: "description", content: "Example for students" }],
+  htmlAttrs: {
+    lang: "en",
+  },
+});
+</script>
